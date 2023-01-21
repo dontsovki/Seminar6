aac376f3a7a506a4e9299bdfcc79f6fb3ea7ea90
Artem.don <dontsovki@gmail.com>
2023-01-21 13:24:46 +0300

    Программа с функциями и методами, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ Task5/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа с функциями и методами, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int[] CopyArray(int[] inArray) { int[] result = new int[inArray.Length]; 
+for (int i = 0; i < inArray.Length; i++) { result[i] = inArray[i]; } return result; }
+
+
